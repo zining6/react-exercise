568c74f2bb50db64140dab849c323ad0ad529207
zining wang <zining6@ualberta.ca>
2022-11-13 08:22:19 -0700

    basepart

---
+++ todolist/src/components/List/index.jsx
@@ -0,0 +1,15 @@
+import React, {Component} from 'react'
+import './index.css'
+import Item from '../Item'
+
+
+export default class List extends Component{
+	render(){
+		return(
+		<ul className="todo-main">
+		  <Item/>
+		 
+		</ul>
+		)
+	}
+	}
