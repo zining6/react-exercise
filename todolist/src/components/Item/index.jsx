c2ed16fbd644189d5319a301b7c676a57a4cb3d2
zining wang <zining6@ualberta.ca>
2022-11-16 07:17:51 -0700

    all partdone

--- todolist/src/components/Item/index.jsx
+++ todolist/src/components/Item/index.jsx
@@ -1,18 +1,79 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
 import './index.css'
 
+export default class Item extends Component {
 
-export default class Item extends Component{
-	render(){
+	state = {mouse:false} //标识鼠标移入、移出
+
+	//鼠标移入、移出的回调
+	handleMouse = (flag)=>{
+		return ()=>{
+			this.setState({mouse:flag})
+		}
+	}
+
+	//勾选、取消勾选某一个todo的回调
+	handleCheck = (id)=>{
+		return (event)=>{
+			this.props.updateTodo(id,event.target.checked)
+		}
+	}
+
+	//删除一个todo的回调
+	handleDelete = (id)=>{
+		if(window.confirm('确定删除吗？')){
+			this.props.deleteTodo(id)
+		}
+	}
+
+
+	render() {
 		const {id,name,done} = this.props
-		return(
-		<li>
-		  <label>
-		    <input type="checkbox"/>
-		    <span>{name}</span>
-		  </label>
-		  <button className="btn btn-danger" style={{display:'none'}}>删除</button>
-		</li>
+		const {mouse} = this.state
+		return (
+			<li style={{backgroundColor:mouse ? '#ddd' : 'white'}} onMouseEnter={this.handleMouse(true)} onMouseLeave={this.handleMouse(false)}>
+				<label>
+					<input type="checkbox" checked={done} onChange={this.handleCheck(id)}/>
+					<span>{name}</span>
+				</label>
+				<button onClick={()=> this.handleDelete(id) } className="btn btn-danger" style={{display:mouse?'block':'none'}}>删除</button>
+			</li>
 		)
 	}
-	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import React, {Component} from 'react'
+// import './index.css'
+
+
+// export default class Item extends Component{
+// 	render(){
+// 		const {id,name,done} = this.props
+// 		return(
+// 		<li>
+// 		  <label>
+// 		    <input type="checkbox"/>
+// 		    <span>{name}</span>
+// 		  </label>
+// 		  <button className="btn btn-danger" style={{display:'none'}}>删除</button>
+// 		</li>
+// 		)
+// 	}
+// 	}
